7d6db29e2d2556bba8af1f42722a6c5e2144a87a
임서영 <sylim0318@naver.com>
2021-09-15 22:29:28 +0900

    [ADD]

---
+++ src/main/java/Tamchack/tamchack/TamchackApplication.java
@@ -0,0 +1,13 @@
+package Tamchack.tamchack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamchackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamchackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tamchack/tamchack/TamchackApplicationTests.java
@@ -0,0 +1,13 @@
+package Tamchack.tamchack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamchackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
